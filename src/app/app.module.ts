b02c0ffac19c1c95fde74b93021c37b781d3de02
ng-proto <n.g.prakash99@gmail.com>
2016-11-25 00:51:26 +0530

    MetroUI updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { NavbarComponent } from './navbar/navbar.component';
+const appRoutes: Routes = [
+    { 
+      path: '', 
+      component: DashboardComponent 
+    },
+    { 
+      path: 'dashboard', 
+      component: DashboardComponent 
+    },
+    { 
+      path: 'detailView', 
+      component: DetailViewComponent 
+    }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DetailViewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
